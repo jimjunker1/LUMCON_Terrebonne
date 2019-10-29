5378a74f1125ce7241167ead7adbd931ce2e91b4
jimjunker1 <james.junker1@gmail.com>
2019-10-29 14:21:05 -0500

    initial package update

--- install-packages.R
+++ install-packages.R
@@ -2,9 +2,10 @@
 if ("pacman" %in% rownames(installed.packages()) == FALSE) install.packages("pacman")
 
 # Install packages required for analysis
-# Add any packages required for you data cleaning and manipulation to the
+# Add any packages required for your data cleaning and manipulation to the
 # `p_load` function call below
 # Do not remove the packages already listed here
 # they are important for running the livedat repository
+# OG package list: pacman::p_load(git2r, httr, semver, testthat, yaml)
 
-pacman::p_load(git2r, httr, semver, testthat, yaml)
+pacman::p_load(git2r, httr, semver, testthat, yaml, tidyverse)
