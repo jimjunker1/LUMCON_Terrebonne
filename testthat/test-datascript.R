9d80c4cac006afca4a0207b094cec8b233d2b954
jimjunker1 <james.junker1@gmail.com>
2020-04-15 21:58:08 -0500

    updated the tests to datascript

--- testthat/test-datascript.R
+++ testthat/test-datascript.R
@@ -3,7 +3,7 @@ context("checks that datascript is working")
 
 test_that("printed value is correct", {
   
-  expect_output(str(source("../datascript.R")), 
+  expect_output(str(source("./datascript.R")), 
                    "Have this script run whatever data cleaning you do")
 
 })
