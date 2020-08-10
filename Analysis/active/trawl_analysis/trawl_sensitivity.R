cc5f5e01e8681ac3e4fb38356b125eb5c9276cc5
jimjunker1 <james.junker1@gmail.com>
2020-08-10 09:20:58 -0500

    updated salinity and temp files and

--- Analysis/active/trawl_analysis/trawl_sensitivity.R
+++ Analysis/active/trawl_analysis/trawl_sensitivity.R
@@ -1,3 +1,5 @@
+source("datascript.R")
+
 ## trawl sampling sensitivity analsis ##
 # Questions are:
 # 1) how many trawls should be done over the year?
@@ -5,8 +7,7 @@
 # 3) evenly? clumped? etc.
 
 # import the data
-TB_trawl_taxasite <- TB_trawl_data %>%
-  filter(year(Date) != "2020") %>% 
+TB_trawl_taxasite <- TB_trawl_data %>% 
 select(Date, Common_name, Abundance) %>%
   group_by(Date, Common_name) %>%
   summarise(Abundance = sum(Abundance)) %>%
@@ -14,18 +15,32 @@ select(Date, Common_name, Abundance) %>%
   pivot_wider(names_from = Common_name, values_from = Abundance, values_fill = list(Abundance = 0)) %>%
   ungroup()
 
-TB_trawl_richness_yr <- TB_trawl_taxasite %>% 
+TB_trawl_richness_yr <- TB_trawl_taxasite %>%
   mutate(year = year(Date),
          month = month(Date)) %>% 
+  filter(year != 2020) %>%
+  filter(month %ni% c(4,5)) %>%
   select(Date, year, month, everything()) %>%
   named_group_split(year)
 
 TB_trawl_richness <- TB_trawl_data %>% na.omit %>% mutate(year = year(Date)) %>%
+  filter(year != 2020) %>% 
   select(year, Common_name) %>% distinct %>%
-  group_by(year) %>% count
+  group_by(year) %>% count %>% bind_rows(data.frame(year= 2020, n = 75))
+
+richness_2020 <- TB_trawl_taxasite %>% na.omit %>% mutate(year = year(Date)) %>%
+  filter(year == 2020) %>% select(-year) %>%
+  pivot_longer(cols = -Date, names_to = "Common_name", values_to = "Abundance") %>%
+  filter(Abundance > 0 & !is.na(Abundance)) %>%
+  select(Date, Common_name) %>% distinct %>%
+  group_by(Date) %>% count %>% 
+  rename(trawl_richness = n) %>% ungroup %>%
+  mutate(trawls = 1:n()) %>% select(-Date) %>%
+  mutate(rel_richness = trawl_richness/75*100)
 
 
-sample_n <- as.list(1:24)
+sample_n <- as.list(1:36)
+
 set.seed(42)
 trawl_sample_simulation <- lapply(sample_n, function(x) lapply(TB_trawl_richness_yr, dplyr::sample_n, size = x, replace = TRUE))
 
@@ -39,7 +54,7 @@ trawl_sample_simulation <- trawl_sample_simulation %>% bind_rows %>%
     pivot_longer(cols = -Date:-trawls, names_to = "Common_name", values_to = "Abundance") %>%
   filter(Abundance > 0 & !is.na(Abundance)) %>% select(-Date, -month, -Abundance) %>%  group_by(year, trawls) %>%
   distinct %>% count %>% rename(trawl_richness = n) %>% left_join(TB_trawl_richness) %>%
-  mutate(rel_richness = trawl_richness/n)
+  mutate(rel_richness = trawl_richness/n*100)
 
 trawl_estimates <- trawl_sample_simulation %>%
   filter(trawls %in% c(9, 15)) %>% group_by(trawls) %>% 
@@ -49,16 +64,19 @@ trawl_estimates <- trawl_sample_simulation %>%
 
 ggplot(trawl_sample_simulation, aes(x = trawls, y = rel_richness, group = factor(year))) + 
   geom_path(aes(color = factor(year)), size = 1.2) +
+  scale_y_continuous(name = "Relative Spp. Richness (%)") +
+  scale_x_continuous(name = "# of trawls")+
   scale_color_viridis(discrete = TRUE) +
   geom_vline(xintercept = c(3,9,15), linetype = c("dashed", rep("solid",2))) +
   annotate('text', x = 2.9, y = 0, label = "Current #", hjust = 1) +
   annotate("text", x = 8.8, y = 0, label = "1 trawl/month", hjust = 1)+
   annotate("text", x = 15.1, y =0, label = "2 trawls/month", hjust = 0) +
-  annotate('text', x = 20, y = 0.3, label = paste0("1 trawl/m =",round(trawl_estimates[1,"mean_rel"],2),"[",
-                                                   round(trawl_estimates[1,"rel_10"],2),"-",
-                                                   round(trawl_estimates[1,"rel_90"],2),"]"), hjust = 0) +
-  annotate('text', x = 20, y = 0.25, label = paste0("2 trawl/m =",round(trawl_estimates[2,"mean_rel"],2),"[",
-                                                    round(trawl_estimates[2,"rel_10"],2),"-",
-                                                    round(trawl_estimates[2,"rel_90"],2),"]"), hjust = 0) +
+  annotate('text', x = 20, y = 35, label = "Total species 'recovery'", hjust = 0, fontface = "bold")+
+  annotate('text', x = 20, y = 30, label = paste0("1 trawl/m = ",round(trawl_estimates[1,"mean_rel"],1)," [ ",
+                                                   round(trawl_estimates[1,"rel_10"],1)," - ",
+                                                   round(trawl_estimates[1,"rel_90"],1)," ]"), hjust = 0) +
+  annotate('text', x = 20, y = 25, label = paste0("2 trawl/m = ",round(trawl_estimates[2,"mean_rel"],1)," [ ",
+                                                    round(trawl_estimates[2,"rel_10"],1)," - ",
+                                                    round(trawl_estimates[2,"rel_90"],1)," ]"), hjust = 0) +
   ggtitle("2020 outlook projections") +
   theme(legend.position = 'none')
