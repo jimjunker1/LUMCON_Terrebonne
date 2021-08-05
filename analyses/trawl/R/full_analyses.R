18a04c5c131ed0af57331a82ebe9dd8a5bfce190
jimjunker1 <jrjunker@mtu.edu>
2021-08-05 15:38:21 -0500

    updated manuscript and workflow.

--- analyses/trawl/R/full_analyses.R
+++ analyses/trawl/R/full_analyses.R
@@ -1,7 +1,10 @@
 # full analysis
 source(here::here('datascript.R'))
+source(here::here("analyses/trawl/R/Lorenz.R"))
 library(hillR)
+nperm = 99
 
+add_row_2008 = data.frame(Date = as.Date("2008-01-01"))
 
 ##create taxa by site matrix
 TB_trawl_taxasite <- TB_trawl_data %>%
@@ -37,6 +40,7 @@ min(TB_trawl_sampling$year, na.rm = TRUE)
 TB_trawl_sampling %>% slice_min(trawl_days) %>% dplyr::select(year) %>% unlist
 # 2011
 
+###### TEMPORAL PATTERNS OF RICHNESS -----------
 #create time-accumulation df from trawl data
 total_richness = length(unique(TB_trawl_data$Common_name));
 #138L
@@ -106,6 +110,37 @@ beta_dates <- gsub('^([0-9]{4}-)([0-9]{1})$', '\\10\\2', names(beta_lists))
 beta_df <- bind_rows(beta_lists) %>% mutate(Date = as.Date(paste0(beta_dates,"-01"), "%Y-%m-%d")) %>% #select(Date, everything()) %>%
   pivot_longer(-Date, names_to = "partition", values_to = "value")
 
+#### TURNOVER ------------
+## quantify loss and appearance of species in the timeseries
+TB_trawl_data %>%
+  # exclude 2020 & 2021
+  dplyr::filter(year %ni% c("2020","2021")) %>%
+  group_by(year, Common_name) %>%
+  dplyr::summarise(Abundance = sum(Abundance, na.rm = TRUE)) %>%
+  group_by(year) %>%
+  pivot_wider( names_from = 'Common_name', values_from = 'Abundance', values_fill = 0) %>%
+  ungroup %>%
+  # dplyr::mutate(time.var = 1:n()) %>%
+  pivot_longer(c(-year), names_to = 'Common_name', values_to = 'Abundance')-> TB_trawl_yr
+  
+trawl_yr_turnover_list = purrr::map(c('appearance','disappearance','total'),
+                                     ~codyn::turnover(df = TB_trawl_yr,
+                                                      time.var = 'year',
+                                                      species.var = 'Common_name',
+                                                      abundance.var = 'Abundance',
+                                                      metric = .x) %>%
+                                       data.frame %>%
+                                       dplyr::mutate(Date = as.Date(paste0(unique(TB_trawl_yr$year)[-1],"-01-01"), format = "%Y-%m-%d"),
+                                                     variable = .x) %>%
+                                       dplyr::rename(value = !!names(.[1])) %>%
+                                       bind_rows(data.frame(value = NA_real_, time.var = NA_real_, Date = as.Date("2008-01-01"), variable =.x))) %>%
+                                     bind_rows
+
+trawl_yr_turnover_list %>%
+  ggplot()+
+  geom_line(aes(x = Date, y = value, group = variable, color = variable), size = 1.1)+
+  scale_color_manual(values = viridis(n = 3, option = 'viridis'))
+  
 
 ##create taxa by site matrix
 TB_trawl_taxasite <- TB_trawl_data %>%
@@ -148,3 +183,122 @@ q2_boots = boot_sample_groups(TB_trawl_taxasite %>% dplyr::select(-julian_date,-
                    group_var = 'year',
                    qD = 2,
                    n_perm = nperm)
+
+q1_boots = boot_sample_groups(TB_trawl_taxasite %>% dplyr::select(-julian_date,-Date,-month,-biweekly), 
+                              group_var = 'year',
+                              qD = 1,
+                              n_perm = nperm)
+
+qD_df = q2_boots %>% 
+  # summarise q2 by year
+  group_by(year) %>%
+  dplyr::summarise(across(q2, list(mean = ~mean(.x, na.rm = TRUE),
+                                   median = ~quantile(.x, 0.5, na.rm = TRUE),
+                                   quant2.5= ~quantile(.x, 0.025, na.rm = TRUE),
+                                   quant25= ~quantile(.x, 0.25, na.rm = TRUE),
+                                   quant75= ~quantile(.x, 0.75, na.rm = TRUE),
+                                   quant97.5= ~quantile(.x, 0.975, na.rm = TRUE)), .names = "{.fn}")) %>%
+  dplyr::mutate(Date = as.Date(paste(year,"01","01", sep = "-"), format = "%Y-%m-%d"),
+                q_order = "q2") %>%
+  bind_rows(
+    q1_boots %>%
+      # summarise q1 by year
+      group_by(year) %>%
+      dplyr::summarise(across(q1, list(mean = ~mean(.x, na.rm = TRUE),
+                                       median = ~quantile(.x, 0.5, na.rm = TRUE),
+                                       quant2.5= ~quantile(.x, 0.025, na.rm = TRUE),
+                                       quant25= ~quantile(.x, 0.25, na.rm = TRUE),
+                                       quant75= ~quantile(.x, 0.75, na.rm = TRUE),
+                                       quant97.5= ~quantile(.x, 0.975, na.rm = TRUE)), .names = "{.fn}")) %>%
+      dplyr::mutate(Date = as.Date(paste(year,"01","01", sep = "-"), format = "%Y-%m-%d"),
+                    q_order = "q1")
+  ) %>%
+  bind_rows(add_row_2008)
+
+
+qD_df %>% dplyr::filter(q_order == 'q2') %>%
+  dplyr::summarise(across(median, ~mean(.x ,na.rm = TRUE))) %>% round(2)
+
+qD_df %>%
+  ggplot()+
+  geom_ribbon(aes(x = Date, ymin = quant2.5, ymax = quant97.5, group = q_order, fill = q_order), alpha = 0.5)+
+  geom_line(aes(x = Date, y = median, group = q_order, color = q_order), size = 1.2)+
+  geom_point(aes(x = Date, y= mean, group = q_order), shape = 6)+
+  scale_y_continuous(name = expression("Order of diversity ("~italic(''^q*D)~")"),limits = c(0,11))+
+  scale_color_manual(labels = c("1","2"), values = viridis(n =2, option = 'viridis'))+
+  scale_fill_manual(labels = c("1","2"),values = viridis(n = 2, alpha = 0.5, option = 'viridis'))+
+  theme(legend.title = element_blank(),
+        legend.position = c(0.01,0.998),
+        legend.justification = c(0,1),
+        )
+
+qeven_df = list(q1_boots,q2_boots) %>%
+  map(~.x %>% group_by(year) %>% slice_sample(n = 1e5, replace = TRUE) %>% dplyr::select(-boot_id)) %>% bind_cols %>%
+  dplyr::select(year = 'year...1', q1,q2,-3) %>%
+  dplyr::mutate(qeven = q1/q2) %>%
+  group_by(year) %>%
+  dplyr::summarise(across(qeven, list(mean = ~mean(.x, na.rm = TRUE),
+                                      median = ~quantile(.x, 0.5, na.rm = TRUE),
+                                      quant2.5= ~quantile(.x, 0.025, na.rm = TRUE),
+                                      quant25= ~quantile(.x, 0.25, na.rm = TRUE),
+                                      quant75= ~quantile(.x, 0.75, na.rm = TRUE),
+                                      quant97.5= ~quantile(.x, 0.975, na.rm = TRUE)), .names = "{.fn}")) %>%
+    dplyr::mutate(Date = as.Date(paste(year,"01","01", sep = "-"), format = "%Y-%m-%d"))
+
+qeven_df %>%
+ggplot()+
+  geom_ribbon(aes(x = Date, ymin = quant2.5, ymax = quant97.5), alpha = 0.5)+
+  geom_line(aes(x = Date, y = median), size = 1.2)+
+  scale_y_continuous(name = expression("Order of diversity ("~italic(''^q*D)~")"),limits = c(0,3))#+
+  # scale_color_manual(labels = c("1","2"), values = viridis(n =2, option = 'viridis'))+
+  # scale_fill_manual(labels = c("1","2"),values = viridis(n = 2, alpha = 0.5, option = 'viridis'))+
+  # theme(legend.title = element_blank(),
+  #       legend.position = c(0.01,0.998),
+  #       legend.justification = c(0,1),
+  # )
+
+
+
+TB_trawl_evenness = TB_trawl_taxasite %>%
+  split(., seq(1:nrow(.))) %>%
+  purrr::map(~.x[-c(1:5)] %>% 
+                   unlist %>%
+                   gini_even(.)) %>% 
+  bind_rows() %>%
+  bind_cols(TB_trawl_taxasite %>% dplyr::select(julian_date:biweekly),.)
+
+# gini_plot = 
+  
+  TB_trawl_evenness %>%
+  ggplot()+
+  geom_line(aes(x = Date, y = `Non-normalized Gini`))
+
+# debugonce(boot_gini_groups)
+annual_evenness_boots = boot_gini_groups(TB_trawl_taxasite %>% dplyr::select(-julian_date,-Date,-month,-biweekly), 
+                                group_var = 'year',
+                                n_perm = nperm)
+
+annual_evenness_df = annual_evenness_boots %>%
+  dplyr::select(-boot_id) %>%
+  pivot_longer(-year, names_to = 'measure', values_to = 'value') %>%
+  group_by(year,measure) %>%
+  dplyr::summarise(across(value, list(mean = ~mean(.x, na.rm = TRUE),
+                                                median = ~quantile(.x, 0.5, na.rm = TRUE),
+                                                quant2.5= ~quantile(.x, 0.025, na.rm = TRUE),
+                                                quant25= ~quantile(.x, 0.25, na.rm = TRUE),
+                                                quant75= ~quantile(.x, 0.75, na.rm = TRUE),
+                                                quant97.5= ~quantile(.x, 0.975, na.rm = TRUE)), .names = "{.fn}")) %>%
+  dplyr::mutate(Date = as.Date(paste(year,"01","01", sep = "-"), format = "%Y-%m-%d"))
+
+annual_evenness_df %>%
+  dplyr::filter(measure == 'gini_raw') %>%
+  ggplot()+
+  geom_ribbon(aes(x = Date, ymin = quant2.5, ymax = quant97.5), alpha = 0.5) +
+  geom_line(aes(x = Date, y = median), size = 1.1) +
+  geom_point(aes(x = Date, y = mean), shape = 22, size = 3)
+
+  # dplyr::mutate(gini_even = purrr::map(.,~.x %>% 
+  #                                            dplyr::select(-julian_date:-biweekly) %>%
+  #                                            unlist %>%
+  #                                            gini_even(.)
+  #                                            ))
--- packages.R
+++ packages.R
@@ -16,7 +16,7 @@ pacman::p_load(git2r, httr, semver, testthat, yaml, here, EML, data.table,
                gridExtra, ggridges, iNEXT, vegan, rlist, pipeR,
                viridis, broom, bbmle, ggthemes, ggeffects, bsts,
                rfishbase, auk, betapart, plot3D, ncdf4, forecast,
-               taxize)
+               taxize, hillR, codyn, gridExtra)
 
 devtools::install_github(c("jimjunker1/junkR"))#, "ropensci/rfishbase"))
 # library(junkR)
@@ -59,6 +59,39 @@ boot_sample_groups = function(abund_mat, group_var, qD, n_perm,...) {
   boot_list = purrr::map(1:n_perm, ~sample_hill(boot_n = .x)) %>% bind_rows
   return(boot_list)
 }
+
+# bootstrapped sampling for estimating variability withing groups
+boot_gini_groups = function(abund_mat, group_var, n_perm,...) {
+  boot_list = vector('list', n_perm)
+  
+  # sample rows and calculate abundance vector
+  sample_even = function(boot_n,...){
+      sample_dat = by(abund_mat, INDICES = abund_mat[,group_var], FUN = sample_frac,
+                    replace = TRUE)
+    class(sample_dat) = "list"
+    sample_dat = bind_rows(sample_dat)
+    
+    agg_groups = stats::aggregate(sample_dat[,-1], by = sample_dat[,1],
+                                  FUN = 'sum')
+    
+    group_col = agg_groups[,group_var]
+    boot_col = paste0("boot", parse(text = boot_n))
+    agg_dat = agg_groups[,-1]
+    
+    dat_groups =  agg_dat %>%
+      apply(., 1, gini_even) %>%
+      t %>% data.frame %>%
+      bind_cols(group_col,.) %>%
+      setNames(., c(group_var, 'gini_raw','gini_norm')) %>%
+      dplyr::mutate(boot_id = boot_col)
+    
+    return(dat_groups)
+  }
+  # debugonce(sample_even)
+  boot_list = purrr::map(1:n_perm, ~sample_even(boot_n = .x)) %>% bind_rows
+  return(boot_list)
+}
+
 
 # yday_summary <<- function(data,...){
 #   if("yday" %in% colnames(data)){
